a387ab37caacb1f06dd4b563fa0fadc7c340a534
Brent Putman <putmanb@georgetown.edu>
2016-10-17 00:02:55 +0000

    OSJ-155: Consider changes to decoders vis-a-vis Content-Type header eval
    Add Content-Type logging.

--- opensaml-messaging-api/src/main/java/org/opensaml/messaging/decoder/servlet/BaseHttpServletRequestXMLMessageDecoder.java
+++ opensaml-messaging-api/src/main/java/org/opensaml/messaging/decoder/servlet/BaseHttpServletRequestXMLMessageDecoder.java
@@ -63,6 +63,8 @@ public abstract class BaseHttpServletRequestXMLMessageDecoder<MessageType extend
     public void decode() throws MessageDecodingException {
         log.debug("Beginning to decode message from HttpServletRequest");
         
+        log.debug("HttpServletRequest indicated Content-Type: {}", getHttpServletRequest().getContentType());
+        
         super.decode();
         
         logDecodedMessage();
