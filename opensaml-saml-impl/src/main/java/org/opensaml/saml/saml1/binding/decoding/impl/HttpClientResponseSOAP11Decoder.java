bcf76c951849f0d424035fc0c28062f7f23e0dde
Scott Cantor <cantor.2@osu.edu>
2017-04-12 14:13:22 -0400

    Fix typo in message.

--- opensaml-saml-impl/src/main/java/org/opensaml/saml/saml1/binding/decoding/impl/HttpClientResponseSOAP11Decoder.java
+++ opensaml-saml-impl/src/main/java/org/opensaml/saml/saml1/binding/decoding/impl/HttpClientResponseSOAP11Decoder.java
@@ -83,7 +83,7 @@ public class HttpClientResponseSOAP11Decoder
         populateBindingContext(getMessageContext());
         
         SAMLObject samlMessage = getMessageContext().getMessage();
-        log.debug("Decoded SOAP messaged which included SAML message of type {}", samlMessage.getElementQName());
+        log.debug("Decoded SOAP message which included SAML message of type {}", samlMessage.getElementQName());
     }
     
     /**
